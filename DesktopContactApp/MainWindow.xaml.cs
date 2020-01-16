b26f19ded4eb3a9dece800ea896a4afbb7015de2
Raúl López <raul.lopez@ingenoba.com>
2020-01-16 12:38:23 +0100

    Add project files.

---
+++ DesktopContactApp/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using DesktopContactApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        
+        {
+            List<Contact> contacts;
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                // En "var contacts " tendremos una lista de objetos "Contact"
+                 contacts = conn.Table<Contact>().ToList();
+
+            }
+            if (contacts != null) 
+            {
+                //foreach (var c in contacts) 
+                //{
+
+                //    contactsListView.Items.Add(new ListViewItem()
+                //    {
+
+                //        Content = c
+
+                //    });
+                //}
+
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+    }
+}
